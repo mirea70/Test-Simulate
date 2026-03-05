e56f46d55342a0bfe388c78175f46aeacbce6890
mirea70 <deae670@daum.net>
2026-03-05 20:14:28 +0900

    project first commit

---
+++ src/main/java/com/simulate/TestSimulateApplication.java
@@ -0,0 +1,13 @@
+package com.simulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSimulateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSimulateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simulate/TestSimulateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSimulateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
